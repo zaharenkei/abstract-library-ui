c6f5470cd1c1a72aba124f07f889e81a3c1c13db
Zaharenko Evginii <zaharenkei@gmail.com>
2023-05-16 17:59:21 +0400

    - dynamic selector update

--- src/Components/Selector/SelectorBlock.js
+++ src/Components/Selector/SelectorBlock.js
@@ -3,18 +3,45 @@ import './Selector.css';
 
 class SelectorBlock extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            categories: [],
+            isLoading: true
+        };
+    }
+
+    async initCategories() {
+        try {
+            const url = "https://5065939d-4db5-4c81-a416-ed91e25b1e42.mock.pstmn.io/categories/";
+            await fetch(url)
+                .then((response) => response.json())
+                .then((jsonData) => this.setState({ categories: jsonData.categories }))
+        } catch (error) {
+            console.log(error);
+        } finally {
+            this.setState({ isLoading: false });
+        }
+
+    }
+
+    componentDidMount() {
+        this.initCategories();
+    }
+
     render() {
+        const {categories} = this.state;
+        console.log(categories);
+        console.log(Array.isArray(categories));
+
+        let components = []; 
+        for (let category of categories){
+            components.push(<div id="selectorItem" ownid="{category.id}" parentid="{category.parentId}"><a href="#">{category.name}</a></div>);
+        }
+
         return (
             <div id="selectorBlock" className="selector">
-                <div id="selectorItem">
-                    <a href="#">Items</a>
-                </div>
-                <div id="selectorItem">
-                    <a href="#">Spells</a>
-                </div>
-                <div id="selectorItem">
-                    <a href="#">Creatures</a>
-                </div>
+                {components}
             </div>
         );
     }
